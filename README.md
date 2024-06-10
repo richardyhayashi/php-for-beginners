72ba7eeafadd6e464127dba9089cf616d42a212b
Richard Y. Hayashi <richardyhayashi@gmail.com>
2024-06-10 19:47:36 +0900

    Ep. 5 completed

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
 # PHP For Beginners
 
+## Docker Compose
+
+### Start
+
+`$ docker compose up --build -d`
+
+### Stop
+
+`$ docker compose down`
+
 ## PHP Code
 
 `<?php echo "Hello World"; ?>`
@@ -11,7 +21,7 @@
 2. PHP For Beginners, Ep 2 - Tools of the Trade
 3. PHP For Beginners, Ep 3 - Your First PHP Tag
 4. PHP For Beginners, Ep 4 - Variables
-5.
+5. PHP For Beginners, Ep 5 - Conditionals and Booleans
 6.
 7.
 8.
--- php-app/index.php
+++ php-app/index.php
@@ -4,14 +4,30 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Demo</title>
+    <style>
+        body {
+            display: grid;
+            place-items: center;
+            height: 100vh;
+            margin: 0;
+            font-family: san-serif;
+        }
+    </style>
 </head>
 <body>
+    <?php
+        $name = "Dark Matter";
+        $read = true;
+
+        if ($read) {
+            $message = "You have read $name";
+        } else {
+            $message = "You have NOT read $name";
+        }
+    ?>
     <h1>
-        <?php
-            $greeting = "Hello";
-            echo $greeting . ' ' . 'World!';
-            echo "$greeting Everybody!";
-        ?>
+        <?php echo $message ?>
+        <?= $message ?>
     </h1>
 </body>
 </html>
